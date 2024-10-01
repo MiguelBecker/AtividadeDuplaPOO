cd0b5a59d9c79470c2ce4c90d6377e53574a974c
MIGUEL CHAVES BECKER <mcb4@cesar.school>
2024-10-01 09:12:29 -0300

    Atualização RepositorioTransação

--- RepositorioTransacao.java
+++ RepositorioTransacao.java
@@ -7,12 +7,14 @@ import java.io.FileWriter;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.time.LocalDate;
-import java.io.File;
+import java.util.List;
+import java.util.ArrayList;
+import java.time.LocalDateTime;
 import br.com.cesarschool.poo.titulos.entidades.Acao;
 import br.com.cesarschool.poo.titulos.entidades.Transacao;
-import br.com.cesarschool.poo.titulos.entidades.Ativo;
 import br.com.cesarschool.poo.titulos.entidades.EntidadeOperadora;
 import br.com.cesarschool.poo.titulos.entidades.TituloDivida;
+
 
 public class RepositorioTransacao {
 	public void incluir(Transacao transacao) throws IOException  {
@@ -27,27 +29,55 @@ public class RepositorioTransacao {
         writer.close();
 	}
 	
-	public Transacao[] buscarPorEntidadeCredora(int identificadorEntidadeCredito) {
-		File arquivo = new File("Acao.txt");
-		BufferedReader reader = new BufferedReader(new FileReader(arquivo));
-        String linha;
-        while((linha = reader.readLine()) != null){
-        	String[] data = linha.split(";");
-        	int id = Integer.parseInt(data[0]);
-        	if(id == identificadorEntidadeCredito) {
-        		String nomeCredito = data[1];
-        		boolean autorizadoCredito = Boolean.parseBoolean(data[2]);
-        		double saldoCredito = Double.parseDouble(data[3]);
-        		double tituloCredito = Double.parseDouble(data[4]);
-        		EntidadeOperadora entidadeCredito = new EntidadeOperadora(id, nomeCredito, autorizadoCredito);
-        		entidadeCredito.creditarSaldoAcao(saldoCredito);
-        		entidadeCredito.creditarSaldoTituloDivida(tituloCredito);
-                Transacao[] transacao = new Transacao();
-                reader.close();
-                return transacao;
-        	}
-        }
-        reader.close();
-        return null;
+	public Transacao[] buscarPorEntidadeCredora(int identificadorEntidadeCredito) throws IOException {
+	    File arquivo = new File("Acao.txt");
+	    BufferedReader reader = new BufferedReader(new FileReader(arquivo));
+	    List<Transacao> transacoes = new ArrayList<>();
+	    String linha;
+
+	    while ((linha = reader.readLine()) != null) {
+	        String[] data = linha.split(";");
+	        int id = Integer.parseInt(data[0]);
+	        if (id == identificadorEntidadeCredito) {
+	            int idCredito = Integer.parseInt(data[0]);
+	            String nomeCredito = data[1];
+	            boolean autorizadoCredito = Boolean.parseBoolean(data[2]);
+	            double saldoCredito = Double.parseDouble(data[3]);
+	            double tituloCredito = Double.parseDouble(data[4]);
+	            EntidadeOperadora entidadeCredito = new EntidadeOperadora(idCredito, nomeCredito, autorizadoCredito);
+	            entidadeCredito.creditarSaldoAcao(saldoCredito);
+	            entidadeCredito.creditarSaldoTituloDivida(tituloCredito);
+
+	            int idDebito = Integer.parseInt(data[5]);
+	            String nomeDebito = data[6];
+	            boolean autorizadoDebito = Boolean.parseBoolean(data[7]);
+	            double saldoDebito = Double.parseDouble(data[8]);
+	            double tituloDebito = Double.parseDouble(data[9]);
+	            EntidadeOperadora entidadeDebito = new EntidadeOperadora(idDebito, nomeDebito, autorizadoDebito);
+	            entidadeDebito.creditarSaldoAcao(saldoDebito);
+	            entidadeDebito.creditarSaldoTituloDivida(tituloDebito);
+
+	            int idAcao = Integer.parseInt(data[10]);
+	            String nomeAcao = data[11];
+	            LocalDate validadeAcao = LocalDate.parse(data[12]);
+	            double valorUnitarioAcao = Double.parseDouble(data[13]);
+	            Acao acao = new Acao(idAcao, nomeAcao, validadeAcao, valorUnitarioAcao);
+
+	            int idTitulo = Integer.parseInt(data[14]);
+	            String nomeTitulo = data[15];
+	            LocalDate validadeTitulo = LocalDate.parse(data[16]);
+	            double valorUnitarioTitulo = Double.parseDouble(data[17]);
+	            TituloDivida tituloDivida = new TituloDivida(idTitulo, nomeTitulo, validadeTitulo, valorUnitarioTitulo);
+
+	            double valorOperacao = Double.parseDouble(data[18]);
+	            LocalDateTime dataHoraOperacao = LocalDateTime.parse(data[19]);
+
+	            Transacao transacao = new Transacao(entidadeCredito, entidadeDebito, acao, tituloDivida, valorOperacao, dataHoraOperacao);
+	            transacoes.add(transacao);
+	        }
+	    }
+	    
+	    reader.close();
+	    return transacoes.toArray(new Transacao[0]);
 	}
 }
